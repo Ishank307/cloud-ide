1b34213870339a7fb5a3447d05e0cb9e46022968
Ishank307 <ishankkumar307@gmail.com>
2025-10-08 15:48:55 +0530

    Fix OAuth callback URL to use HTTPS in production

--- server/config/passport.js
+++ server/config/passport.js
@@ -4,7 +4,9 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
+    callbackURL: process.env.NODE_ENV === 'production' 
+        ? "https://cloud-ide-production.up.railway.app/auth/google/callback"
+        : "/auth/google/callback",
     scope: ['profile', 'email']
 }, async (accessToken, refreshToken, profile, done) => {
     try {
@@ -16,7 +18,7 @@ passport.use(new GoogleStrategy({
             name: profile.displayName,
             picture: profile.photos[0].value
         };
-        
+
         return done(null, user);
     } catch (error) {
         return done(error, null);
