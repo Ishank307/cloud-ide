95d1cdd20ebad5bfe77f5d64fe4c3d1ca0027aa2
Ishank307 <ishankkumar307@gmail.com>
2025-10-08 15:43:25 +0530

    Fix Google OAuth scope parameter in passport configuration

--- server/config/passport.js
+++ server/config/passport.js
@@ -4,7 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "/auth/google/callback",
+    scope: ['profile', 'email']
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         // Here you would typically save user to database
